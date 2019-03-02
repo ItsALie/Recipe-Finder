51d72c82ff6dcdf4e17c7c69b728a5b9b0e04eda
balparmar <bparmar14@my.bcit.ca>
2019-03-02 14:44:09 -0800

    added a scripts folder, with my scripts

---
+++ scripts/seed_data.sql
@@ -0,0 +1,69 @@
+#
+# TABLE STRUCTURE FOR: users
+#
+
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (1, 'christian26', '39e4c255a2890a88c7f9092e6c8a861e6514b62e');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (2, 'qschroeder', '2b725128ce74f3a239327a30fbf2f182e1f4efdc');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (3, 'pouros.bret', '345eb6979db7a1838dfeae8279d4cd944d21d192');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (4, 'thompson.reuben', 'ed25d700422dbffb7e3bad9859233d389f9f3443');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (5, 'kathryne.roob', '093b9bcb607b999d54c0af511dd3632aeceef790');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (6, 'freeda48', '1875b61f552d53907c9ea5a4f7e69a91cb27a206');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (7, 'hammes.mavis', 'afebbd129cadec07e925ecc1ab320b4f7d003989');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (8, 'rboyer', '428c57fea5aadb159d648903ee5be510a63cd189');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (9, 'rcormier', '08c3d7ada54b4da279bba657ff06946e0a0302d8');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (10, 'qbreitenberg', 'cbc09a485b4500ee64c964efb3c70c3e2c6311fe');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (11, 'sylvia33', '66426fff5a56fbd8eff91bc4bbc95980ca64b168');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (12, 'effie.wolf', 'fb80939f0a72647a69ed2daca659612f9d207ef5');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (13, 'collins.mariela', 'c514a9b6214873889e2a4025accf7e8b77a05c85');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (14, 'lindgren.kelly', '8c3c046ea25761fda8995fa45ddd8ecb174f6dbf');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (15, 'nolan29', 'ff3f1d12a1f45bde912665ab5491423369232a24');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (16, 'vergie.armstrong', '519ed5c173eb61ee28fc0f9a76feee75fd55964e');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (17, 'gerald62', 'ba2fbd2e9a9b474f8f3a780ab8061a61497dccb5');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (18, 'teagan62', '484d70031f499b3ece8cc0b5bc2f86fc9247d31d');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (19, 'darlene.stanton', '7a333105e99c8ff9ca30ac232acbee5c0e4cecc8');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (20, 'cole.dashawn', '7649cbc6a10fde4525806764d0ca16b53641e8d7');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (21, 'yswift', 'f47287124395dd85f383105ca38e326fecd6f937');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (22, 'murray.sandra', '021a3bc44ac2451958cc60e184d8f264531b30f3');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (23, 'qromaguera', '8629bd757e36aca407f0da079b5cb1bad1b22707');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (24, 'russel.dawson', '3d8f2f4ff1ddcc361f16e234e4b6e000825d6bfd');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (25, 'garett82', 'c2f7e622c83c4dc316b1ce8bd605cb200436f5bc');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (26, 'kavon79', '2e9e3b0d2f9c7440357c2370da3d58a817b9164f');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (27, 'o\'reilly.dustin', '0c8710a24b8ed52cbf9cb00da3eee4e27628a2d0');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (28, 'raquel83', 'd8423e25f522046e296b007d9dcafe4d9888eb4e');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (29, 'felicia50', '1614520810741d897377a5cc349d2c2ad6d98771');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (30, 'brandi.walter', 'eb57ed093509b01df719438f94f671b431930492');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (31, 'tromp.briana', '56f8fd9173731950755509ddd4a8dae92fcbc97c');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (32, 'nbednar', '6830e4154e26401fd2623ebfa8bd23b4efbe79f7');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (33, 'mmcclure', '9ba85e2058555c8d333851bb8cbdcb2eb9fc5839');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (34, 'solon.kiehn', '427faf476d1df30318b64602d8a631c7bd2a9018');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (35, 'cathryn.stracke', 'd454c44c7e537bf2ff1e7a878b3edd76d3e2b95b');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (36, 'volkman.olaf', '7ff1091b322b98a6f17a437e229d08e016561fb4');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (37, 'kaylie38', '9c709cf2fd88bb3a26cd3971bad2f8fb1267eba7');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (38, 'mherman', '7d19e91c06143098fe48f7ec761e149cfa1e04f4');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (39, 'naomie71', '402790608eb2f9a7eb7079cd8b49493fab09c807');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (40, 'ellis01', 'fe4e62cc8073ab03af13afbda5c98f6439c02d4e');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (41, 'crona.gillian', '62abf9f6d699fea937bd924992067ff3ba796324');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (42, 'adell61', 'f1860e5bd76121e59176750361b85c1471d515ff');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (43, 'gislason.brittany', 'fa97ef63325c620140969ebba2dab87e4df0288e');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (44, 'camden.gerhold', '8c6e878a54d12bdea532c57dc050a80cf66f7e50');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (45, 'raphael.breitenberg', 'd826a296195aeff012ac9863893de4a9d635e0b7');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (46, 'oklocko', 'c25e8b1d6d2de4b3f32f222482a1a14bf601d00a');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (47, 'gina01', '53829a1f5a17210e4589f4cfefa70634759e6ba2');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (48, 'shana14', '4726b6529901622c9491c2934661f8014abbac82');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (49, 'izabella11', 'fcfc266ccaaf509bf7c09005836996006e193677');
+INSERT INTO `users` (`user_id`, `username`, `password`) VALUES (50, 'mdouglas', 'aae84d9a108e57371ad9bc7a5400530b7f9796df');
+
+
+INSERT INTO "recipes" ("recipe_id", "user_id", "serving_size", "name") VALUES (1, 1, "2 servings", "Lasagna");
+INSERT INTO "recipes" ("recipe_id", "user_id", "serving_size", "name") VALUES (2, 2, "3 servings", "Burgers");
+INSERT INTO "recipes" ("recipe_id", "user_id", "serving_size", "name") VALUES (3, 3, "5 servings", "Salads");
+INSERT INTO "recipes" ("recipe_id", "user_id", "serving_size", "name") VALUES (4, 4, "7 servings", "Tacos");
+INSERT INTO "recipes" ("recipe_id", "user_id", "serving_size", "name") VALUES (5, 5, "1 servings", "Burrito");
+
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (1, 1, 1, "That lasagna looks delicious!");
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (1, 1, 1, "Could I substitute the cheese for Daya?!");
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (2, 2, 2, "Those burgers looks delicious!");
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (3, 3, 3, "That salad looks delicious!");
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (4, 4, 4, "These tacos looks delicious!");
+INSERT INTO "comments" ("comments_id", "user_id", "recipe_id", "comments") VALUES (5, 5, 5, "That burrito looks delicious!");
+
