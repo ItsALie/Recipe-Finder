0ece16a3c7539b5c7bd04b3aa11b8b9f222cb15d
Haley <haley1652@hotmail.com>
2019-03-01 21:59:49 -0800

    created default pages - adding routing between pages

--- recipe_finder/routes/index.js
+++ recipe_finder/routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Recipe Finder' });
 });
 
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Search' });
+});
+
+router.get('/recipes', function(req, res, next) {
+  res.render('recipes', { title: 'Recipes' });
+});
+
+router.get('/recipe_details', function(req, res, next) {
+  res.render('recipe_details', { title: 'Recipe Information' });
+});
+
 module.exports = router;
