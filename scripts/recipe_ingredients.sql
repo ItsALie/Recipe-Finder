b90441268fc4b58d7391f44497cca29639908526
Parker <chenjianming697@gmail.com>
2019-03-05 14:39:45 -0800

    updated recipe_ingredients

--- scripts/recipe_ingredients.sql
+++ scripts/recipe_ingredients.sql
@@ -9,4 +9,12 @@ FOREIGN KEY (ingredients_id) REFERENCES ingredients(ingredients_id),
 quantity VARCHAR(40) NOT NULL
 );
 
-INSERT INTO recipes (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (1, 1, 1, "1 cup");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (1, 1, 1, "1 cup");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (2, 1, 1, "1 kg");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (3, 1, 1, "1 pang");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (4, 1, 1, "1 grain");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (5, 1, 1, "1 gram");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (6, 1, 1, "1 miliigram");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (7, 1, 1, "2 cup");
+INSERT INTO recipe_ingredients (recipe_ingredients_id, recipe_id, ingredients_id, quanitity) VALUES (8, 1, 1, "3 cup");
+
