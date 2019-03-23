c4bf0716cab90756f030f7188463880394f5a6ee
Parker <chenjianming697@gmail.com>
2019-03-23 14:34:45 -0700

    updated again

--- scripts/recipes_table.sql
+++ scripts/recipes_table.sql
@@ -12,8 +12,9 @@ name VARCHAR(40) NOT NULL
 /* Inseting the dummy data to sql table*/
 INSERT INTO recipes (recipe_id, user_id, serving_size, name) VALUES (1, 1, "1.1", "fire rice");
 
-/* update the recipes name */
+/* Update script to modify recipe names*/
+/* Modifu should only work if the logged in user matches the recipe user*/
 UPDATE recipes SET name = 'noodles' WHERE recipe_id = 1 AND user_id = 1;
 
 /* Delete Recipe from the Recipe table */
-DELETE FROM recipes WHERE recipe_id = 1;
+DELETE FROM recipes WHERE recipe_id = 1 AND user_id = 1;
