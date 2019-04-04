ce243620cdb5afd0b5d850646c29297119257fdd
Michael Vu <mvu14@my.bcit.ca>
2019-04-04 14:42:58 -0700

    Including compiled scripts

---
+++ scripts/select_script.sql
@@ -0,0 +1,6 @@
+
+-- Select user
+SELECT user_id from users WHERE user_id = 1 AND password = '39e4c255a2890a88c7f9092e6c8a861e6514b62e' ;
+
+-- Select all recipe
+SELECT * FROM recipes WHERE recipe_id="";
