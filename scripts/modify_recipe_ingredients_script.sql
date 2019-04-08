0bd6a6c465334eefda919273250539bda1eee7be
mikevu23 <43010987+mikevu23@users.noreply.github.com>
2019-04-08 14:09:29 -0700

    Delete modify_recipe_ingredients_script.sql

--- scripts/modify_recipe_ingredients_script.sql
+++
@@ -1,8 +0,0 @@
-USE recipeFinderDatabase;
-
-
-UPDATE recipe_ingredients AS ingre
-INNER JOIN recipes AS reci ON reci.recipe_id = ingre.recipe_id
-SET ingre.ingredients_id=1,
-ingre.quantity = "2 kg"
-WHERE reci.user_id=2 AND ingre.recipe_id=2 AND ingre.ingredients_id=2;
