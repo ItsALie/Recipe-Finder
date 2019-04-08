7ce2b7c1ab2127c6390d6684ebf10d54db459da9
JianmingChen <43011142+JianmingChen@users.noreply.github.com>
2019-04-08 14:05:25 -0700

    Update delete_script.sql

--- scripts/delete_script.sql
+++ scripts/delete_script.sql
@@ -7,3 +7,7 @@ DELETE FROM ingredient WHERE user_id = ;
 DELETE FROM comments WHERE user_id =;
 DELETE FROM recipes WHERE user_id =;
 DELETE FROM users WHERE user_id = ;
+
+/* Deletes corresponding recipe_ingredient table rows that have a matching recipe_ id */
+
+DELETE FROM recipe_ingredients WHERE recipe_id = 1;
