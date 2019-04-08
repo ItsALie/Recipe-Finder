87723da42a6ffbc46757ce273b3c842048d2063f
mikevu23 <43010987+mikevu23@users.noreply.github.com>
2019-04-08 13:53:36 -0700

    Update delete_script.sql

--- scripts/delete_script.sql
+++ scripts/delete_script.sql
@@ -2,8 +2,8 @@
 DELETE FROM comments WHERE comments_id="" AND user_id="";
 
 -- Delete user
-DELETE FROM recipe_ingredients WHERE user_id = 1;
-DELETE FROM ingredient WHERE user_id = 1;
-DELETE FROM comments WHERE user_id =1;
-DELETE FROM recipes WHERE user_id =1;
-DELETE FROM users
+DELETE FROM recipe_ingredients WHERE user_id = ;
+DELETE FROM ingredient WHERE user_id = ;
+DELETE FROM comments WHERE user_id =;
+DELETE FROM recipes WHERE user_id =;
+DELETE FROM users WHERE user_id = ;
