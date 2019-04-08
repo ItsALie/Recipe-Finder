39ef4f3dd22e4504d82df6bd902833a9cea17a59
JianmingChen <43011142+JianmingChen@users.noreply.github.com>
2019-04-08 14:14:08 -0700

    Update select_script.sql

--- scripts/select_script.sql
+++ scripts/select_script.sql
@@ -6,6 +6,6 @@ SELECT user_id from users WHERE user_id = 1 AND password = '39e4c255a2890a88c7f9
 SELECT * FROM recipes WHERE recipe_id="";
 
 -- Joining two tables and selecting name of the ingredient and quanitity from two tables where the ingredient id is equal
--- creating script to get recipe ingredients and steps from recipe id
+-- creating script to get recipe ingredients from recipe id
 
 SELECT I.name FROM ingredients AS I INNER JOIN recipe_ingredients AS RI ON I.ingredients_id = RI.ingredients_id
