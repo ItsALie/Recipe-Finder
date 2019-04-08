5b5d139b3094033ed84c3b8635b814b187e7b8f8
JianmingChen <43011142+JianmingChen@users.noreply.github.com>
2019-04-08 14:01:52 -0700

    Update update_script.sql

--- scripts/update_script.sql
+++ scripts/update_script.sql
@@ -7,3 +7,7 @@ INNER JOIN recipes AS reci ON reci.recipe_id = ingre.recipe_id
 SET ingre.ingredients_id=1,
 ingre.quantity = "2 kg"
 WHERE reci.user_id=2 AND ingre.recipe_id=2 AND ingre.ingredients_id=2;
+
+/* Update script to modify recipe names*/
+/* Modify should only work if the logged in user matches the recipe user*/
+UPDATE recipes SET name = 'noodles' WHERE recipe_id = 1 AND user_id = 1;
